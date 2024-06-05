41f83e3ee6e25239e6cdc34385507d833697a9a3
armandosoaress <linkssoares123@gmail.com>
2024-06-05 20:03:01 -0300

    dsfasdf

--- financeiro/api/atualizaresumo.php
+++ financeiro/api/atualizaresumo.php
@@ -4,7 +4,9 @@ header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: text/html; charset=utf-8');
 include_once 'conexao.php';
-$dados = json_decode(file_get_contents('php://input'), true);
+// $dados = json_decode(file_get_contents('php://input'), true);
+// pegar via get
+$dados = $_GET;
 
 $valor = $dados['valor'];
 $valor = str_replace(',', '.', $valor);

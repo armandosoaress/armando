a6649d73e3fc9db60a9765b327b0bc0468445ba5
armandosoaress <linkssoares123@gmail.com>
2024-06-05 20:17:51 -0300

    asdf

--- financeiro/js/page.js
+++ financeiro/js/page.js
@@ -1,21 +1,21 @@
-function showPage(page) {
-    if (page == 'organizador') {
-        document.querySelector('.menu_organizador').style.backgroundColor = 'white';
-        document.querySelector('.menu_init').style.backgroundColor = 'transparent';
-    }
-    if (page == 'init') {
-        document.querySelector('.menu_init').style.backgroundColor = 'white';
-        document.querySelector('.menu_organizador').style.backgroundColor = 'transparent';
-    }
+// function showPage(page) {
+//     if (page == 'organizador') {
+//         document.querySelector('.menu_organizador').style.backgroundColor = 'white';
+//         document.querySelector('.menu_init').style.backgroundColor = 'transparent';
+//     }
+//     if (page == 'init') {
+//         document.querySelector('.menu_init').style.backgroundColor = 'white';
+//         document.querySelector('.menu_organizador').style.backgroundColor = 'transparent';
+//     }
 
-    document.querySelectorAll('.pagina').forEach(element => {
-        element.style.display = 'none';
-    });
-    document.querySelector('.' + page).style.display = 'flex';
-}
-document.querySelectorAll('.pagina').forEach(element => {
-    element.style.display = 'flex';
-});
+//     document.querySelectorAll('.pagina').forEach(element => {
+//         element.style.display = 'none';
+//     });
+//     document.querySelector('.' + page).style.display = 'flex';
+// }
+// document.querySelectorAll('.pagina').forEach(element => {
+//     element.style.display = 'flex';
+// });
 
 
-document.querySelector('.organizador').style.display = 'none';
+// document.querySelector('.organizador').style.display = 'none';

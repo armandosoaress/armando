99390e4f11c5c17b74b5d45f2d376a4fc8bb64ea
armandosoaress <linkssoares123@gmail.com>
2024-06-05 20:11:01 -0300

    dsf

--- financeiro/api/atualizaresumo.php
+++ financeiro/api/atualizaresumo.php
@@ -5,10 +5,11 @@ header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: text/html; charset=utf-8');
 include_once 'conexao.php';
 $dados = json_decode(file_get_contents('php://input'), true);
+
 $valor = $dados['valor'];
-$valor = str_replace(',', '.', $valor);
 $created_at = date('Y-m-d');
 $tipo = $dados['tipo'];
+
 $sql = "INSERT INTO " . $tipo . " (valor, created_at) VALUES ('$valor', '$created_at')";
 if (mysqli_query($conexao, $sql)) {
     echo json_encode(array(

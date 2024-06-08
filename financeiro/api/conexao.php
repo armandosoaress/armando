17d2335b19f19b4eecf2122dbf543d9c92a37000
armandosoaress <linkssoares123@gmail.com>
2024-06-08 10:17:14 -0300

    dfsdf

--- financeiro/api/conexao.php
+++ financeiro/api/conexao.php
@@ -3,15 +3,12 @@ $servidor = "localhost";
 $usuario = "arman008_armando";
 $senha = "Arma312FJN$";
 $banco = "arman008_armando";
-
-// $servidor = "localhost";
-// $usuario = "root";
-// $senha = "";
-// $banco = "testeaa";
-
-
-
-
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "testeaa";
+}
 $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
 if (!$conexao) {
     die("Falha na conexao: " . mysqli_connect_error());

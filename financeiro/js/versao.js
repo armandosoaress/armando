052c32b2fb64cdbdcdb6d067736ac9504451c93a
armandosoaress <linkssoares123@gmail.com>
2024-06-09 13:42:30 -0300

    Update version display with formatted timestamp

--- financeiro/js/versao.js
+++ financeiro/js/versao.js
@@ -8,7 +8,11 @@ const apiUrl = `https://api.github.com/repos/${owner}/${repo}/actions/runs`;
 fetch(apiUrl)
     .then(response => response.json())
     .then(data => {
-        document.getElementById('version').innerHTML = "V.." + data.total_count;
+        console.log(data.workflow_runs[0].created_at);
+        // converter 2024-06-09T16:40:25Z para 09/06/2024 13:40:25
+        let dataCriacao = new Date(data.workflow_runs[0].created_at);
+        let dataFormatada = dataCriacao.toLocaleDateString() + " " + dataCriacao.toLocaleTimeString();
+        document.getElementById('version').innerHTML = "V:" + data.total_count + " - " + dataFormatada;
     })
     .catch(error => {
         console.error('Erro ao obter informações sobre as últimas actions:', error);
